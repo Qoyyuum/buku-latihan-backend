a2c1e5110ba36d290742d5c0d0544e16943fe851
Abdul Qoyyuum <abdul.qoyyuum@gmail.com>
2021-06-11 22:53:49 +0800

    :rocket: Fiddled with logging for Heroku deploy

--- config/settings/production.py
+++ config/settings/production.py
@@ -14,7 +14,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["satu2ti.ga"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["buku-latihan.herokuapp.com"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
@@ -137,7 +137,7 @@ COMPRESS_FILTERS = {
 
 LOGGING = {
     "version": 1,
-    "disable_existing_loggers": True,
+    "disable_existing_loggers": False,
     "formatters": {
         "verbose": {
             "format": "%(levelname)s %(asctime)s %(module)s "
