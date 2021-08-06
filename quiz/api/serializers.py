e39c421fce679b07c6c05b2fb9cfd4af3c560631
Abdul Qoyyuum <abdul.qoyyuum@gmail.com>
2021-08-06 17:21:49 +0800

    :rotating_light: Pre-committed all files

--- quiz/api/serializers.py
+++ quiz/api/serializers.py
@@ -1,37 +1,39 @@
 from rest_framework import serializers
-from quiz.models import Category, SubCategory, Quiz, Progress, Sitting, Question
+
+from quiz.models import Category, Progress, Question, Quiz, Sitting, SubCategory
+
 
 class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
 
-	class Meta:
-		model = Category
-		fields = '__all__'
 
 class SubCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubCategory
+        fields = "__all__"
 
-	class Meta:
-		model = SubCategory
-		fields = '__all__'
 
 class QuizSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Quiz
+        fields = "__all__"
 
-	class Meta:
-		model = Quiz
-		fields = '__all__'
+
 class ProgressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Progress
+        fields = "__all__"
 
-	class Meta:
-		model = Progress
-		fields = '__all__'
 
 class SittingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sitting
+        fields = "__all__"
 
-	class Meta:
-		model = Sitting
-		fields = '__all__'
 
 class QuestionSerializer(serializers.ModelSerializer):
-
-	class Meta:
-		model = Question
-		fields = '__all__'
+    class Meta:
+        model = Question
+        fields = "__all__"
--- quiz/api/views.py
+++ quiz/api/views.py
@@ -1,56 +1,73 @@
+from rest_framework.mixins import CreateModelMixin, ListModelMixin, RetrieveModelMixin
 from rest_framework.viewsets import GenericViewSet
-from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, CreateModelMixin
-from quiz.models import Quiz, Question, Category, SubCategory, Progress, Sitting
-from quiz.api.serializers import QuizSerializer, QuestionSerializer, CategorySerializer, SubCategorySerializer, ProgressSerializer, SittingSerializer
+
+from quiz.api.serializers import (
+    CategorySerializer,
+    ProgressSerializer,
+    QuestionSerializer,
+    QuizSerializer,
+    SittingSerializer,
+    SubCategorySerializer,
+)
+from quiz.models import Category, Progress, Question, Quiz, Sitting, SubCategory
+
 # from rest_framework.response import Response
 # from rest_framework import status
 
-class QuizViewSet(RetrieveModelMixin, ListModelMixin, GenericViewSet):
-	queryset = Quiz.objects.all()
-	serializer_class = QuizSerializer
 
-	def get(self, request, *args, **kwargs):
-		return self.list(request, *args, **kwargs)
+class QuizViewSet(RetrieveModelMixin, ListModelMixin, GenericViewSet):
+    queryset = Quiz.objects.all()
+    serializer_class = QuizSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
 
 class CategoryViewSet(RetrieveModelMixin, ListModelMixin, GenericViewSet):
-	queryset = Category.objects.all()
-	serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
 
-	def get(self, request, *args, **kwargs):
-		return self.list(request, *args, **kwargs)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
 
 class SubCategoryViewSet(RetrieveModelMixin, ListModelMixin, GenericViewSet):
-	queryset = SubCategory.objects.all()
-	serializer_class = SubCategorySerializer
+    queryset = SubCategory.objects.all()
+    serializer_class = SubCategorySerializer
 
-	def get(self, request, *args, **kwargs):
-		return self.list(request, *args, **kwargs)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
 
 class QuestionViewSet(RetrieveModelMixin, ListModelMixin, GenericViewSet):
-	queryset = Question.objects.all()
-	serializer_class = QuestionSerializer
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
 
-	def get(self, request, *args, **kwargs):
-		return self.list(request, *args, **kwargs)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-class ProgressViewSet(RetrieveModelMixin, CreateModelMixin, ListModelMixin, GenericViewSet):
-	queryset = Progress.objects.all()
-	serializer_class = ProgressSerializer
 
-	def get(self, request, *args, **kwargs):
-		return self.list(request, *args, **kwargs)
+class ProgressViewSet(
+    RetrieveModelMixin, CreateModelMixin, ListModelMixin, GenericViewSet
+):
+    queryset = Progress.objects.all()
+    serializer_class = ProgressSerializer
 
-	def post(self, request, *args, **kwargs):
-		return self.create(request, *args, **kwargs)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-class SittingViewSet(RetrieveModelMixin, CreateModelMixin, ListModelMixin, GenericViewSet):
-	queryset = Sitting.objects.all()
-	serializer_class = SittingSerializer
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
-	def get(self, request, *args, **kwargs):
-		return self.list(request, *args, **kwargs)
 
-	def post(self, request, *args, **kwargs):
-		return self.create(request, *args, **kwargs)
+class SittingViewSet(
+    RetrieveModelMixin, CreateModelMixin, ListModelMixin, GenericViewSet
+):
+    queryset = Sitting.objects.all()
+    serializer_class = SittingSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
